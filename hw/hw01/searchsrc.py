2daecb87fe215825d8decb6cd0ca80de45f62027
Ian Setia <isetia@student13.cse.nd.edu>
2024-08-31 16:58:07 -0400

    HW01 - Added readFile and countInclude functions to searchsrc.py

--- hw/hw01/searchsrc.py
+++ hw/hw01/searchsrc.py
@@ -1,2 +1,40 @@
 #Ian Setia
 #isetia@nd.edu
+
+import argparse
+
+def readFile(fileName):
+    f = open(fileName, 'r')
+    lines = []
+    while True:
+        line = f.readline()
+        if len(line) == 0:
+            break
+        lines.append(line)
+    return lines
+
+def countInclude(lines):
+    i = 0
+    for line in lines:
+        if line.startswith('#include'):
+            i+=1
+    return i
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('fileName', type=str, help='name of file to scan')
+    parser.add_argument('--include', action='store_true', help='counts number of include statements in code')
+    args = parser.parse_args()
+
+    lines = readFile(args.fileName)
+    
+    print("file: {} lines: {}".format(args.fileName, len(lines)), end=' ')
+
+    if args.include:
+        print("include: {}".format(countInclude(lines)), end=' ');
+
+    print("")
+
+    
+if __name__ == "__main__":
+    main()
