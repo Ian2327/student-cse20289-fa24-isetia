8f5f9b6993c3b8ec66d6b47a3c0db8f7e167e16d
Ian Setia <isetia@student13.cse.nd.edu>
2024-08-31 17:23:30 -0400

    HW01 - Added countMember, countPtr functions and added comments to prior code

--- hw/hw01/searchsrc.py
+++ hw/hw01/searchsrc.py
@@ -3,6 +3,7 @@
 
 import argparse
 
+#Reads a given file and returns a list with the lines in the file
 def readFile(fileName):
     f = open(fileName, 'r')
     lines = []
@@ -13,6 +14,7 @@ def readFile(fileName):
         lines.append(line)
     return lines
 
+#Scans each line in the list of lines for the #include
 def countInclude(lines):
     i = 0
     for line in lines:
@@ -20,10 +22,38 @@ def countInclude(lines):
             i+=1
     return i
 
+#Counts number of :: substrings present in every line, returns sum
+def countMember(lines):
+    m = 0
+    for line in lines:
+        m+=line.count('::')
+    return m
+
+#Similar to countMember (searches for -> instead of ::) returns pointer count
+def countPtr(lines):
+    p = 0
+    for line in lines:
+        p+=line.count('->')
+    return p
+    
+
+def countSimplefunc(lines):
+    return 100
+    
+def countSimplefuncec(lines):
+    return 100
+
 def main():
     parser = argparse.ArgumentParser()
+
+    #adds argument tags to identify from command line
     parser.add_argument('fileName', type=str, help='name of file to scan')
     parser.add_argument('--include', action='store_true', help='counts number of include statements in code')
+    parser.add_argument('--member', action='store_true', help='')
+    parser.add_argument('--ptr', action='store_true', help='')
+    parser.add_argument('--simplefunc', action='store_true', help='')
+    parser.add_argument('--simplefuncec', action='store_true', help='')
+
     args = parser.parse_args()
 
     lines = readFile(args.fileName)
@@ -31,7 +61,15 @@ def main():
     print("file: {} lines: {}".format(args.fileName, len(lines)), end=' ')
 
     if args.include:
-        print("include: {}".format(countInclude(lines)), end=' ');
+        print("include: {}".format(countInclude(lines)), end=' ')
+    if args.member:
+        print("member: {}".format(countMember(lines)), end=' ')
+    if args.ptr:
+        print("ptr: {}".format(countPtr(lines)), end=' ')
+    if args.simplefunc:
+        print("simplefunc: {}".format(countSimplefunc(lines)), end=' ')
+    if args.simplefuncec:
+        print("simplefuncec: {}".format(countSimplefuncec(lines)), end=' ')
 
     print("")
 
